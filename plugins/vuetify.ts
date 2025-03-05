87a37915a4c2f3497b206ef8b362ad864e164d0f
mycbxzd1 <mycbxzd@163.com>
2025-03-05 16:43:04 +0800

    :art: vuetify was introduced to fix the white edge issue

---
+++ plugins/vuetify.ts
@@ -0,0 +1,16 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { defineNuxtPlugin } from '#app'
+
+// 引入 Vuetify 的样式
+import 'vuetify/styles'
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const vuetify = createVuetify({
+        components,
+        directives,
+    })
+
+    nuxtApp.vueApp.use(vuetify)
+})
